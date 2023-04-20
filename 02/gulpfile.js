62fe532eb0a7f566da86f776b2f0d433cc7337f0
ahmadrzq <ahmadrizqiisnain@gmail.com>
2023-04-20 16:47:10 +0700

    add template

---
+++ 02/gulpfile.js
@@ -0,0 +1,31 @@
+// List dependencies
+const { src, dest, watch, series } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const prefix = require("gulp-autoprefixer");
+const minify = require("gulp-clean-css");
+const terser = require("gulp-terser");
+
+// Create Functions
+
+// SCSS
+function compileScss() {
+  return src("dev/scss/**/*.scss")
+    .pipe(sass())
+    .pipe(prefix("last 2 versions"))
+    .pipe(minify())
+    .pipe(dest("dist/css"));
+}
+
+// JS
+function jsmin() {
+  return src("dev/js/*.js").pipe(terser()).pipe(dest("dist/js"));
+}
+
+// Create Watchtask
+function watchtask() {
+  watch("dev/scss/**/*.scss", compileScss);
+  watch("dev/js/*.js", jsmin);
+}
+
+// Default gulp
+exports.default = series(compileScss, jsmin, watchtask);
